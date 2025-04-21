eea95fd6d70d4b3b0c9380a5a84e05a012b0c455
Khrystel Ricci <krpenaflor2002@gmail.com>
2025-04-21 11:50:47 +0800

    install icon

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,13 @@ import LoginView from '@/views/LoginView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/register',
+    //   name: 'register',
+    //   component: RegisterView,
+    // },
     {
-      path: '/register',
-      name: 'register',
-      component: RegisterView,
-    },
-    {
-      path: '/login',
+      path: '/',
       name: 'login',
       component: LoginView,
     },
